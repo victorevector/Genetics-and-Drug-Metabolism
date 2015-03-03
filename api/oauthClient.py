43aa0993674d4bc048bed4c15e357352ddef012c
Victor E <victorevector@gmail.com>
2015-03-02 22:12:34 -0800

    initalized api app. api app will replace token_retriever.

---
+++ api/oauthClient.py
@@ -0,0 +1,16 @@
+import requests
+import settings
+
+class OAuthClient(object):
+
+    def __init__(self):
+        pass
+
+    def get_auth(self):
+        pass
+        
+    def get_token(self):
+        pass
+
+    def refresh_token(self):
+        pass
