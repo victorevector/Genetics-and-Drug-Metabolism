af3f17d7d31c5bdd011bd0a0a633107e2bfd8c3b
Victor E <victorevector@gmail.com>
2015-03-30 17:47:49 -0700

    Deleted the app, 'token_retriever,' as it is no longer necessary. All functionality has been transferred and integrated into the app, 'api'

--- token_retriever/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from token_retriever import views
-
-urlpatterns = patterns('',
-    url(r'^index/', views.index, name = 'index'),
-    url(r'^url_constructor/', views.url_constructor, name = 'url_constructor'),
-    url(r'^callback/', views.callback, name = 'callback'),
-    )
